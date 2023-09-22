9481d34d1eb5208b95578fa39a1d9add140d3f97
Burak <145280474+Burakstr@users.noreply.github.com>
2023-09-22 15:16:33 +0300

    Add files via upload

---
+++ Dört Renk Uygulaması/Dört Renk Uygulaması/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Dört Renk Uygulaması
+//
+//  Created by Burak Satır on 21.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
